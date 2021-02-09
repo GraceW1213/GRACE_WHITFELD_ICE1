f358b6b1fc54d3457d5f4ca49318f0fc52118484
Grace Whitfield <78034712+GraceW1213@users.noreply.github.com>
2021-02-09 18:14:55 -0500

    Adding lucky card

--- src/cardtrick/CardTrick.java
+++ src/cardtrick/CardTrick.java
@@ -20,7 +20,12 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
+        card[] luckyCard = new Card[1];
+        for(int i=0; i<luckyCard.length; i++){
+            Card c = new Card();
+            c.setValue(13);
+            c.setSuit("Hearts");
+        }
         
         for (int i=0; i<magicHand.length; i++)
         {
