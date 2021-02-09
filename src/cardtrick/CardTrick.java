2e20180232313e636d54d5fe42051363c5c45838
Grace <Grace@192.168.2.11>
2021-02-09 13:53:01 -0500

    writing most of code

--- src/cardtrick/CardTrick.java
+++ src/cardtrick/CardTrick.java
@@ -5,6 +5,8 @@
  */
 package cardtrick;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -12,6 +14,8 @@ package cardtrick;
  * @author Grace Whitfield
  */
 public class CardTrick {
+    
+    public static Scanner input = new Scanner(System.in);
     
     public static void main(String[] args)
     {
@@ -21,12 +25,27 @@ public class CardTrick {
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+           int value = (int)(Math.floor(Math.random()*13)+1);
+            c.setValue(value);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
+            int rnd = (int)Math.round(Math.random()*3);
+                      
+            c.setSuit(Card.SUITS[rnd]);
+            
+            System.out.println(magicHand[i].value);
+        }// end of for loop
         
         //insert code to ask the user for Card value and suit, create their card
+        System.out.println("Please enter a number between 1-13: ");
+        int number = input.nextInt();
+        System.out.println("Please enter a suit for your card: ");
+        String suit = input.nextLine();
         // and search magicHand here
+        boolean match = false;
+        for (int i=0; i<magicHand.length;i++){
+            
+        }
         //Then report the result here
-    }
+    }//end of main method
     
 }
